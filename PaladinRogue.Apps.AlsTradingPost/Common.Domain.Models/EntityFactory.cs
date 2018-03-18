53ac8f371324a47127ac8347cd7ef54c035f24f3
ch3n3y <dan.cheney87@gmail.com>
2018-03-18 22:38:51 +0000

    Create entity factory (#6)

---
+++ PaladinRogue.Apps.AlsTradingPost/Common.Domain.Models/EntityFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Common.Domain.Models
+{
+    public static class EntityFactory
+    {
+        public static T CreateEntity<T>()
+        {
+            return (T)Activator.CreateInstance(typeof(T));
+        }
+    }
+}
