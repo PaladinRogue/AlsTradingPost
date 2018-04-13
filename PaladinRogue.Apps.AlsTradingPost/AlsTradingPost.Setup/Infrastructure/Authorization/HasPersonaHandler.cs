a8466af06730d1aeafb7d5b8f913e207a52f94d6
Dan Cheney <dan.cheney87@gmail.com>
2018-04-13 18:16:55 +0100

    Persona authorisation

---
+++ PaladinRogue.Apps.AlsTradingPost/AlsTradingPost.Setup/Infrastructure/Authorization/HasPersonaHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using AlsTradingPost.Resources;
+using Microsoft.AspNetCore.Authorization;
+using JwtClaims = AlsTradingPost.Resources.Constants.JwtClaims;
+
+namespace AlsTradingPost.Setup.Infrastructure.Authorization
+{
+    public class HasPersonaHandler : AuthorizationHandler<HasPersonaRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasPersonaRequirement requirement)
+        {
+            if (context.User.HasClaim(c =>
+            {
+                Enum.TryParse(c.Value, out Persona persona);
+                return c.Type == JwtClaims.Persona &&
+                       persona == requirement.Persona;
+            }))
+            {
+                context.Succeed(requirement);
+
+                return Task.CompletedTask;
+            }
+            context.Fail();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ PaladinRogue.Apps.AlsTradingPost/AlsTradingPost.Setup/Infrastructure/Authorization/PersonaPolicies.cs
@@ -0,0 +1,8 @@
+﻿namespace AlsTradingPost.Setup.Infrastructure.Authorization
+{
+    public static class PersonaPolicies
+    {
+        public const string Admin = "Persona:Admin";
+        public const string Player = "Persona:Player";
+    }
+}
