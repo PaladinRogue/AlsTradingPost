e622f7f7b46dafeef77839838d3b2d60cac564e3
Dan Cheney <dan.cheney87@gmail.com>
2018-05-08 13:03:54 +0100

    Add table restrictions to Authentication and Sessions

--- PaladinRogue.Apps.AlsTradingPost/Authentication.Domain.Models/Identity.cs
+++ PaladinRogue.Apps.AlsTradingPost/Authentication.Domain.Models/Identity.cs
@@ -1,9 +1,11 @@
-﻿using Common.Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Common.Domain.Models;
 
 namespace Authentication.Domain.Models
 {
     public class Identity : VersionedEntity
     {
+        [MaxLength(50)]
         public string AuthenticationId { get; set; }
     }
 }
--- PaladinRogue.Apps.AlsTradingPost/Authentication.Persistence/Migrations/20180508115727_AddTableRestrictionsAuthentication.Designer.cs
+++ PaladinRogue.Apps.AlsTradingPost/Authentication.Persistence/Migrations/20180508115727_AddTableRestrictionsAuthentication.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Authentication.Persistence.Migrations
 {
     [DbContext(typeof(AuthenticationDbContext))]
-    partial class AuthenticationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180508115727_AddTableRestrictionsAuthentication")]
+    partial class AddTableRestrictionsAuthentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
