5bdb2f16258ca6856c5cb10cdd12debd520ee45d
Tom <Thomas.Ryder@gmail.co.uk>
2018-05-10 22:44:35 +0100

    added the status controller, and holy shit this solution is incredible now!!!

---
+++ PaladinRogue.Apps.AlsTradingPost/AlsTradingPost.Api/Controllers/StatusController.cs
@@ -0,0 +1,26 @@
+﻿using AlsTradingPost.Api.Status;
+using Common.Api.Builders.Resource;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlsTradingPost.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatusController : Controller
+    {
+        private readonly IResourceBuilder _resourceBuilder;
+
+        public StatusController(IResourceBuilder resourceBuilder)
+        {
+            _resourceBuilder = resourceBuilder;
+        }
+
+        [HttpGet]
+        [Route("", Name = RouteDictionary.Status)]
+        public IActionResult Get()
+        {
+            return new ObjectResult(
+                _resourceBuilder.Create(new StatusResource()).Build()
+            );
+        }
+    }
+}
