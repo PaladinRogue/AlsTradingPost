5a09dcd135077794f32384276ec36d1f45193e98
Thomas Ryder <thomas.ryder@gmail.com>
2018-05-14 12:27:18 +0100

    added status controller  to authentication

--- PaladinRogue.Apps.AlsTradingPost/Authentication.Api/Controllers/AuthenticationController.cs
+++ PaladinRogue.Apps.AlsTradingPost/Authentication.Api/Controllers/AuthenticationController.cs
@@ -137,5 +137,5 @@ namespace Authentication.Api.Controllers
 					.Build()
 			);
 		}
-	}
+    }
 }
--- PaladinRogue.Apps.AlsTradingPost/Authentication.Api/RouteDictionary.cs
+++ PaladinRogue.Apps.AlsTradingPost/Authentication.Api/RouteDictionary.cs
@@ -2,6 +2,7 @@
 {
     public static class RouteDictionary
     {
+        public const string Status = "Status";
         public const string AuthenticationServices = "AuthenticationServices";
         public const string AuthenticationFacebook = "AuthenticationFacebook";
         public const string AuthenticationFacebookTemplate = "AuthenticationFacebookTemplate";
