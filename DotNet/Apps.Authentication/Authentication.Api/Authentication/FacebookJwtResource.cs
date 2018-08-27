e3434ab560b55a530fca9a5e86ea2cc194fcdcb0
ch3n3y <dan.cheney87@gmail.com>
2018-08-27 14:27:21 +0100

    Restructure folders for dotnet and web (#61)
    * Restructure folders for dotnet and web
    
    * Remove old git ignore

---
+++ DotNet/Apps.Authentication/Authentication.Api/Authentication/FacebookJwtResource.cs
@@ -0,0 +1,13 @@
+﻿using Common.Api.Meta;
+using Common.Api.Validation.Attributes;
+
+namespace Authentication.Api.Authentication
+{
+    public class FacebookJwtResource : JwtResource
+    {
+        [ReadOnly]
+        [Hidden]
+        [Required]
+        public string AccessToken { get; set; }
+    }
+}
